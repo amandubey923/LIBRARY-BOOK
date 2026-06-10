9e7436da626189df440888ea0c653422a7994e45
amandubey923 <kumaraman19137@gmail.com>
2026-06-10 15:51:24 +0530

    Refactor book data structure for improved readability and maintainability

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,17 +7,24 @@ const guessPopup = document.getElementById('guessPopup');
 
 // 📚 BOOK DATA (same names as files)
 const books = [
-    { name: "1984", link: "pages/1984.html" },
-    { name: "the great gatsby", link: "pages/thegreatgatsby.html" },
-    { name: "crime punishment", link: "pages/crimepunishment.html" },
-    { name: "moby dick", link: "pages/mobydick.html" },
-    { name: "one hundred", link: "pages/onehundred.html" },
-    { name: "war and peace", link: "pages/warandpeace.html" },
-    { name: "pride and prejudice", link: "pages/prideandprejudice.html" },
-    { name: "lord of ring", link: "pages/lordofring.html" },
-    { name: "mockingbird", link: "pages/mockingbird.html" },
-    { name: "godan", link: "pages/godan.html" },
-    { name: "gaban", link: "pages/gaban.html" }
+    { name: "1984", link: "pages/1984.html" },                                      //1
+    { name: "the great gatsby", link: "pages/thegreatgatsby.html" },                //2
+    { name: "crime punishment", link: "pages/crimepunishment.html" },               //3
+    { name: "moby dick", link: "pages/mobydick.html" },                             //4
+    { name: "one hundred", link: "pages/onehundred.html" },                         //5
+    { name: "war and peace", link: "pages/warandpeace.html" },                      //6
+    { name: "pride and prejudice", link: "pages/prideandprejudice.html" },          //7
+    { name: "lord of ring", link: "pages/lordofring.html" },                        //8
+    { name: "mockingbird", link: "pages/mockingbird.html" },                        //9
+    { name: "godan", link: "pages/godan.html" },                                    //10
+    { name: "gaban", link: "pages/gaban.html" },                                    //11
+    { name: "kamayani", link: "pages/kamayani.html" },                              //12
+    { name: "madhushala", link: "pages/madhushala.html" },                          //13
+    { name: "one hundred", link: "pages/onehundred.html" },                         //14
+    { name: "war and peace", link: "pages/warandpeace.html" },                      //15
+    { name: "mailaanchal", link: "pages/mailaanchal.html" },                        //16
+    { name: "raag darbari", link: "pages/raagdarbari.html" },                       //17
+    { name: "itoohadlovestory", link: "pages/itoohadlovesstory.html" },             //18
 ];
 
 // 🔍 FUNCTION → SEARCH + REDIRECT
